46b53bf9142283a5d78e9c1bbeda1fd680b2579b
Tanner Bacon <tannerbacon20@gmail.com>
2023-03-01 00:34:01 -0700

    Adding repository functionality

--- BaconBookstore/Controllers/HomeController.cs
+++ BaconBookstore/Controllers/HomeController.cs
@@ -9,14 +9,17 @@ namespace BaconBookstore.Controllers
 {
     public class HomeController : Controller
     {
+        /* This is being replaced by the repository method
         private BookstoreContext _context { get; set; }
-
         public HomeController (BookstoreContext bookStore) =>  _context = bookStore;
+        */
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository bookstoreRepository) => repo = bookstoreRepository;
 
         public IActionResult Index()
         {
             // Getting books for context
-            var context = _context.Books.ToList();
+            var context = repo.Books.ToList();
 
             return View(context);
         }
