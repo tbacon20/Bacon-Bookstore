57b28b9e29c1cf0a15b95725d74ad1a1969dab74
Tanner Bacon <tannerbacon20@gmail.com>
2023-03-15 21:24:30 -0600

    adding cart icon and cart summary view

---
+++ BaconBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaconBookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaconBookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
